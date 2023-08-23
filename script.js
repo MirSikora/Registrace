b3076e0ad30b4b8c24d07a3da8877b8144946903
Miroslav Sikora <miroslavsikora5@gmail.com>
2023-08-23 10:22:08 +0200

    hamburger icon

--- script.js
+++ script.js
@@ -4,14 +4,18 @@
 
 const menuIcon = document.querySelector(".menu-icon"),
   navbar = document.querySelector(".nav-menu"),
-  body = document.querySelector("body");
+  hamburgerIcon = document.querySelector(".fa-solid");
 
 menuIcon.addEventListener("click", () => {
   if (navbar.classList.contains("active")) {
     navbar.classList.add("disactive");
     navbar.classList.remove("active");
+    hamburgerIcon.classList.remove("fa-xmark");
+    hamburgerIcon.classList.add("fa-bars");
   } else {
     navbar.classList.add("active");
     navbar.classList.remove("disactive");
+    hamburgerIcon.classList.remove("fa-bars");
+    hamburgerIcon.classList.add("fa-xmark");
   }
 });
